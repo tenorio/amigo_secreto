ff965745f58b869ad6e110e38735bd3c0183fcbb
Marcelo Tenorio de Souza <marcelllo.martin@gmail.com>
2018-02-05 18:50:21 -0200

    Add campaign models and their members

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,3 +1,16 @@
 class Campaign < ApplicationRecord
+  before_create :set_status
+  before_create :set_member
   belongs_to :user
+  enum status: [:pending, :finished]
+  has_many :members, dependent: :destroy
+  validates :title, :description, :user, :status, presence: true
+
+  def set_status
+    self.status = :pending
+  end
+
+  def set_member
+    self.members << Member.create(name: self.user.name, email: self.user.email)
+  end
 end
